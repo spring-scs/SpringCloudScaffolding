b8a10f0c57746088574d40e2625703b4e7f1a89e
cxf <cxf@qq.com>
2020-04-09 16:19:02 +0800

    test config

--- scs-config/src/main/java/com/xwzj/scs/ConfigApplication.java
+++ scs-config/src/main/java/com/xwzj/scs/ConfigApplication.java
@@ -3,12 +3,14 @@ package com.xwzj.scs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @Slf4j
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigApplication{
     
 	public static void main( String[] args ){
--- scs-hello/src/main/java/com/xwzj/scs/HelloApplication.java
+++ scs-hello/src/main/java/com/xwzj/scs/HelloApplication.java
@@ -3,22 +3,30 @@ package com.xwzj.scs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ *
+ */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
+@RestController
 @Slf4j
-public class EurakaApplication {
+public class HelloApplication {
+
+    @Value("${spring.datasource.url}")
+    String url;
 
 	public static void main( String[] args ){
-        SpringApplication.run(EurakaApplication.class, args);
-        log.info( "Euraka Start Success..." );
+        SpringApplication.run(HelloApplication.class, args);
+        System.out.println( "HelloApplication Start Success..." );
     }
 
+    @RequestMapping("/get")
+    public String getName(){
+        return url;
+    }
 }
